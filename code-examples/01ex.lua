d0b94199130fc31e8365c7f15ddde69a219077b9
serr <sc7227484@gmail.com>
2025-08-23 21:45:13 +0300

    quick update

---
+++ code-examples/01ex.lua
@@ -0,0 +1,19 @@
+for k in pairs(fs) do print(k) end
+
+--[[
+unlock
+lock_dir
+_DESCRIPTION
+_VERSION
+attributes
+chdir
+currentdir
+dir
+link
+lock
+mkdir
+rmdir
+symlinkattributes
+setmode
+touch
+]]--
